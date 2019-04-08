bfbe5d28a5fb8e02a389e529f0010dbd48ad235e
zhangpeng <dazto@dazhenkeji.cn>
2019-04-08 10:42:12 +0800

    配置文件修改

--- src/main/java/com/dazhenkeji/CbmsApplication.java
+++ src/main/java/com/dazhenkeji/CbmsApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CbmsApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CbmsApplication.class, args);
     }
 
