aec8310f3f03901de0d93493a689f0ed3f150b72
qiaozhinan <qiaobajia@outlook.com>
2019-04-09 14:59:39 +0800

    增加page

---
+++ src/main/java/com/dazhenkeji/cbms/operate_partition/ResultPage.java
@@ -0,0 +1,22 @@
+package com.dazhenkeji.cbms.operate_partition;
+
+public class ResultPage {
+    private Long total;
+    private Object rows;
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Object getRows() {
+        return rows;
+    }
+
+    public void setRows(Object rows) {
+        this.rows = rows;
+    }
+}
